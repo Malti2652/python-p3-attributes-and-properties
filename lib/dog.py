5db5745be3b2fe895a6968509600f2125c9912dd
Malti2652 <eddingtonibi@gmail.com>
2024-10-16 15:17:56 +0300

    attributes

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,19 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+# class Dog:
+#     pass
+
 class Dog:
-    pass
+    def __init__(self, name="Unknown"):
+        self._name = None  
+        self.name = name 
+    def name(self):
+        return self._name
+    
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
