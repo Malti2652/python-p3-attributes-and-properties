e8337fe0ec5cfdeadbf2d24ecbd6e657fcdea965
Malti2652 <eddingtonibi@gmail.com>
2024-10-22 09:57:50 +0300

    attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -14,16 +14,76 @@ APPROVED_BREEDS = [
 # class Dog:
 #     pass
 
+# class Dog:
+#     def __init__(self, name="Unknown"):
+#         self._name = None  
+#         self.name = name 
+#     def name(self):
+#         return self._name
+    
+#     def name(self, value):
+#         if isinstance(value, str) and 1 <= len(value) <= 25:
+#             self._name = value
+#         else:
+#             print("Name must be string between 1 and 25 characters.")
+    
+
+
+# class Dog:
+#     approved_breeds = ["Labrador", "Poodle", "Bulldog", "Beagle", "Pug"]
+
+#     def __init__(self, name="Buddy", breed="Labrador"):
+#         self.name = name  # Use the setter to validate the name
+#         self.breed = breed  # Use the setter to validate the breed
+
+#     @property
+#     def name(self):
+#         return self._name
+
+#     @name.setter
+#     def name(self, value):
+#         if isinstance(value, str) and 1 <= len(value) <= 25:
+#             self._name = value
+#         else:
+#             print("Name must be string between 1 and 25 characters.")
+
+#     @property
+#     def breed(self):
+#         return self._breed
+
+#     @breed.setter
+#     def breed(self, value):
+#         if value in Dog.approved_breeds:
+#             self._breed = value
+#         else:
+#             print("Breed must be in list of approved breeds.")
+#             self._breed = None  # Fallback to prevent uninitialized state
+
 class Dog:
-    def __init__(self, name="Unknown"):
-        self._name = None  
-        self.name = name 
+    def __init__(self, name="Default Name", breed=None):
+        self._name = None
+        self._breed = None
+        self.name = name
+        self.breed = breed
+
+    @property
     def name(self):
         return self._name
-    
+
+    @name.setter
     def name(self, value):
         if isinstance(value, str) and 1 <= len(value) <= 25:
             self._name = value
         else:
             print("Name must be string between 1 and 25 characters.")
-    
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value is None or value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
